6f9bad3bf3617df96f2bf7d25509efd48bbb584c
olegscherbak <nix.001@ukr.net>
2021-10-29 22:33:56 +0300

    save1

--- src/js/refs.js
+++
@@ -1,9 +0,0 @@
-const searchForm = document.querySelector('.search-form');
-const gallery = document.querySelector('.gallery');
-const cardsList = document.querySelector('.gallery');
-
-export default {
-  searchForm,
-  gallery,
-  cardsList,
-};
